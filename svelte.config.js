4a062f2d9781083ab18500de1a5080e7407e0d1a
Travis Horn <travis@travishorn.com>
2025-08-10 11:25:21 -0500

    GitHub Pages deploy fix 2

--- svelte.config.js
+++ svelte.config.js
@@ -11,13 +11,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter(),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
